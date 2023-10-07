435a71f75ec0bda1c9bc593f1e58e5f0d0d40eef
Quentin Froment <fromentquentin@gmail.com>
2023-10-07 17:45:36 +0200

    feat(translate): add calculation between dates for years factor in strings

--- js/age.js
+++ js/age.js
@@ -8,3 +8,14 @@ function getAge(dateString) {
     }
     return age;
 }
+function getYears(start_date,end_date){
+    var startDate = new Date(start_date);
+    var endDate = new Date();
+    if (end_date !==''){
+    endDate = new Date(end_date);
+    }
+    const yearsDifference = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24 * 365.25));
+    // console.log(`The difference in years is approximately ${yearsDifference} years.`);
+    return yearsDifference;
+
+}
